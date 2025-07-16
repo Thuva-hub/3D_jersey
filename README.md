9f98e4842b44d69e7250da6b067663b42bf2e4dd
Charan Romi <s92075569@ousl.lk>
2025-07-16 07:12:25 +0530

    added backend

--- README.md
+++
@@ -1,2 +0,0 @@
-# 3D_jersey
-3d Jersey
---
+++ backend/server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+const { Pool } = require("pg");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+  port: process.env.DB_PORT,
+});
+pool.connect((err) => {
+  if (err) {
+    console.error("Database connection error:", err.stack);
+  } else {
+    console.log("Connected to the database");
+  }
+});
